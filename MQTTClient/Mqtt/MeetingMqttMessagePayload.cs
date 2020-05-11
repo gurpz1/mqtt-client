a98c4436a466861f91c48bd9716e7b58e915f78e
Gurpal Sagoo <gurpz1@gmail.com>
2020-05-11 01:03:35 +0100

    MVP done

---
+++ MQTTClient/Mqtt/MeetingMqttMessagePayload.cs
@@ -0,0 +1,19 @@
+﻿using System.Text.Json;
+using System.Text.Json.Serialization;
+using MQTTClient.Polling.Models;
+
+namespace MQTTClient.Mqtt
+{
+    public class MeetingMqttMessagePayload
+    {
+        public Availability Availability { get; set; }
+
+        public string ToJson()
+        {
+            var options = new JsonSerializerOptions();
+            options.Converters.Add(new JsonStringEnumConverter(JsonNamingPolicy.CamelCase));
+            return JsonSerializer.Serialize(this, options);
+        }
+    }
+    
+}
---
+++ MQTTClient/Mqtt/MqttMessage.cs
@@ -0,0 +1,33 @@
+﻿using MQTTClient.Polling.Models;
+
+namespace MQTTClient.Mqtt
+{
+    public class MqttMessage
+    {
+        public string Topic { get; }
+        public string Payload { get; }
+
+        public MqttMessage(string topic, string payload)
+        {
+            Topic = topic;
+            Payload = payload;
+        }
+        
+        public override string ToString()
+        {
+            return $"{Topic}: {Payload}";
+        }
+
+        public static MqttMessage GenerateMqttMessageForMeetingStatus(string clientId, IApplication application,
+            IMeetingDetails meetingDetails)
+        {
+            var topic = $"stat/{clientId}/meeting/{application.ApplicationName.ToLower()}";
+            var payload = new MeetingMqttMessagePayload()
+            {
+                Availability = meetingDetails.Availability
+            };
+            return new MqttMessage(topic, payload.ToJson());
+        }
+        
+    }
+}
